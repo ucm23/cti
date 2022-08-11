1289008a843a1a0d340728cfcfd7f601a610d680
Canos13 <canomsergio13@gmail.com>
2022-08-11 15:33:31 -0500

    Usamos los ColorButton

--- components/Work.js
+++ components/Work.js
@@ -2,27 +2,38 @@ import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { ColorButton } from '../lib/theme';
+import { Stack } from '@mui/material';
+import { MdExpandMore } from 'react-icons/md';
+import { useRouter } from 'next/router'
 
 const Work = ({titulo, src}) => {
-  return (
-        <Card sx={{ minWidth: 435, maxWidth: 435  }} className='card-margen'>
-            <CardMedia
-                component="img"
-                height="250"
-                image={src}
-                alt={src} />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {titulo}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small" variant='outlined'>Ver Más</Button>
-            </CardActions>
-        </Card>
-  )
+    const { push } = useRouter();
+
+    const handleClick = () => {
+        push('/Soluciones');    
+    }
+
+    return (
+            <Card sx={{ minWidth: 435, maxWidth: 435  }} className='card-margen'>
+                <CardMedia
+                    component="img"
+                    height="250"
+                    image={src}
+                    alt={src} />
+                <CardContent className='d-flex'>
+                    <Typography gutterBottom variant="h5" component="div" className='m-auto font-work'>
+                        {titulo}
+                    </Typography>
+                </CardContent>
+                <CardActions className='pt-0'>
+                    <Stack spacing={3} direction="row" className='mx-auto mb-4'>
+                        <ColorButton onClick={handleClick} variant="contained" endIcon={<MdExpandMore className='ml-2' />}>Leer Más</ColorButton>
+                    </Stack>
+                </CardActions>
+            </Card>
+    )
 }
 
 export default Work
