1cbf2445003d33839b8749746fb89b44a067c48e
Canos13 <canomsergio13@gmail.com>
2022-08-12 17:35:42 -0500

    agregamos la cdn de nprogress

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" rel="stylesheet" />
       </Head>      
       <body>
         <Main />
