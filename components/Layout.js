601d36af6538766a0e0dffb660ff4d26a6707edf
Canos13 <canomsergio13@gmail.com>
2022-08-12 14:59:24 -0500

    useEffect para el nprogress

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,39 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Layout = ({children}) => {
-  return (
-    <>  
-        <Head>
-            <title>CTI </title>
-        </Head>
-        <main>
-            {children}
-        </main>
-    </>
-  )
+  
+    const router = useRouter();
+    useEffect(() => {
+
+        const handleRouteChange = (url) => {
+            console.log(url);
+            NProgress.start();
+        };
+
+        router.events.on('routeChangeStart', handleRouteChange);
+        router.events.on('routeChangeComplete', ()=>{
+            NProgress.done();
+        })
+
+        return () =>{
+            router.events.off('routeChangeStart', handleRouteChange);
+        }
+
+    }, [])
+
+    return (
+      <>  
+          <Head>
+              <title>CTI </title>
+          </Head>
+          <main>
+              {children}
+          </main>
+      </>
+    )
 }
 
 export default Layout
