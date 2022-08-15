0a88f6c4194ee73f0d1a2beaca9ab71cb95055dd
Canos13 <canomsergio13@gmail.com>
2022-08-15 12:58:03 -0500

    agregamos efectos de Fade al navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,48 +1,49 @@
 import Link from 'next/link';
+import { Fade } from "react-awesome-reveal";
 
 const Navbar = () => {
-  return (
-    <div>
-        <nav className="nav container">
-            <div className="nav_logo">
-                <p className="nav_title">Grupo CTI</p>
-            </div>
+    return (
+        <Fade direction="down">
+            <nav className="nav container">
+                <div className="nav_logo">
+                    <p className="nav_title">Grupo CTI</p>
+                </div>
 
-            <ul className="nav_link nav_link_menu">
-                <li className="nav_items">
-                    <Link href="/">
-                        <a className="nav_links">Inicio</a>
-                    </Link>
-                </li>
-                <li className="nav_items">
-                    <Link href="/Nosotros" >
-                        <a className="nav_links">Nosotros</a>
-                    </Link>
-                </li>
-                <li className="nav_items">
-                    <Link href="/Soluciones">
-                        <a className="nav_links">Soluciones</a>
-                    </Link>
-                </li>
-                <li className="nav_items">
-                    <Link href="/Blog">
-                        <a className="nav_links">Blog</a>
-                    </Link>
-                </li>
-                <li className="nav_items">
-                    <Link href="/Contacto">
-                        <a className="nav_links">Contacto</a>
-                    </Link>
-                </li>
-                <img src="/close.svg" className="nav_close" />
-            </ul>
+                <ul className="nav_link nav_link_menu">
+                    <li className="nav_items">
+                        <Link href="/">
+                            <a className="nav_links">Inicio</a>
+                        </Link>
+                    </li>
+                    <li className="nav_items">
+                        <Link href="/Nosotros" >
+                            <a className="nav_links">Nosotros</a>
+                        </Link>
+                    </li>
+                    <li className="nav_items">
+                        <Link href="/Soluciones">
+                            <a className="nav_links">Soluciones</a>
+                        </Link>
+                    </li>
+                    <li className="nav_items">
+                        <Link href="/Blog">
+                            <a className="nav_links">Blog</a>
+                        </Link>
+                    </li>
+                    <li className="nav_items">
+                        <Link href="/Contacto">
+                            <a className="nav_links">Contacto</a>
+                        </Link>
+                    </li>
+                    <img src="/close.svg" className="nav_close" />
+                </ul>
 
-            <div className="nav_menu">
-                <img src="/menu.svg" className="nav_img" />
-            </div>
-        </nav>
-    </div>
-  )
+                <div className="nav_menu">
+                    <img src="/menu.svg" className="nav_img" />
+                </div>
+            </nav>
+        </Fade>
+    )
 }
 
 export default Navbar
