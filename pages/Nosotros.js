11050264832531a0fcbc6908f563ddbec81c4fe9
Canos13 <canomsergio13@gmail.com>
2022-08-16 09:17:42 -0500

    Creamos el componente Nosotros

--- pages/Nosotros.js
+++ pages/Nosotros.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Fade } from 'react-awesome-reveal'
+import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
+import Image from 'next/image'
 
 const Nosotros = () => {
-  return (
-    <div>Nosotros</div>
-  )
+    return (
+        <Layout>
+            <header className='about'>
+              {/* Barra de navegación */}
+              <Navbar />
+
+              {/* Encabezado */}
+              <section className="hero_container container">
+                  <Fade direction="right">
+                      <h1 className="hero_title">
+                          ¿Quiénes somos?
+                      </h1>
+                  </Fade>
+              </section>
+            </header>
+
+            <section className='bg-nosotros'>
+              <section className='nosotros'>
+                  <section className='nosotros-texto'>
+                      <h1>Grupo CTI Tech-IN POS</h1>
+                      <p>Somos un equipo multidisciplinario con más de 20 años de
+                        experiencia en servicios de consultoría especializada en:
+                        Seguridad Informática, Virtualización, Soporte Técnico,
+                        Infraestructura, Análisis y Diseño de Procesos, Políticas,
+                        Registro de Propiedad Intelectual y Desarrollo de Software a
+                        grandes corporativos de los sectores: financiero,
+                        manufactura, construcción, farmacéutico, automotriz,
+                        hospitalario, educativo y gobierno. Además contamos con
+                        Plataformas TI propias.</p>
+                  </section>
+                  <section>
+                      <Image
+                          src="/Person_Image.svg"
+                          alt="Picture of the author"
+                          width={520}
+                          height={520}
+                      />
+                  </section>
+              </section>
+            </section>
+        </Layout>
+    )
 }
 
 export default Nosotros
