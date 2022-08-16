681bb9e9c92816d2f3683693c38a970a11614259
Canos13 <canomsergio13@gmail.com>
2022-08-16 12:33:44 -0500

    importamos el Footer

--- pages/Nosotros.js
+++ pages/Nosotros.js
@@ -2,6 +2,7 @@ import { Fade } from 'react-awesome-reveal'
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Image from 'next/image'
+import Footer from '../components/Footer'
 
 const Nosotros = () => {
     return (
@@ -23,16 +24,8 @@ const Nosotros = () => {
             <section className='bg-nosotros'>
               <section className='nosotros'>
                   <section className='nosotros-texto'>
-                      <h1>Grupo CTI Tech-IN POS</h1>
-                      <p>Somos un equipo multidisciplinario con más de 20 años de
-                        experiencia en servicios de consultoría especializada en:
-                        Seguridad Informática, Virtualización, Soporte Técnico,
-                        Infraestructura, Análisis y Diseño de Procesos, Políticas,
-                        Registro de Propiedad Intelectual y Desarrollo de Software a
-                        grandes corporativos de los sectores: financiero,
-                        manufactura, construcción, farmacéutico, automotriz,
-                        hospitalario, educativo y gobierno. Además contamos con
-                        Plataformas TI propias.</p>
+                      <h1 className='nosotros-titulo'>Grupo CTI Tech-IN POS</h1>
+                      <p className='nosotros-text'>Somos un equipo multidisciplinario con más de 20 años de experiencia en servicios de consultoría especializada en: Seguridad Informática, Virtualización, Soporte Técnico, Infraestructura, Análisis y Diseño de Procesos, Políticas, Registro de Propiedad Intelectual y Desarrollo de Software agrandes corporativos de los sectores: financiero, manufactura, construcción, farmacéutico, automotriz, hospitalario, educativo y gobierno. Además contamos con Plataformas TI propias.</p>
                   </section>
                   <section>
                       <Image
@@ -44,6 +37,21 @@ const Nosotros = () => {
                   </section>
               </section>
             </section>
+
+            <section className='mis-vis'>
+                <section className='bloques-mis'>
+                    <div className='vis'>
+                        <h1>Visión</h1>
+                        <p> Ser el aliado estratégico en Tecnología, que nos permita posicionarnos como el principal referente en soluciones IT y ciberseguridad en México, Estados Unidos, Centro y Sur de América.</p>
+                    </div>
+                    <div className='mis'>
+                        <h1>Misión</h1>
+                        <p>Creamos relaciones de confianza y profesionalismo que nos permite pensar como un solo equipo entre nuestros colaboradores, clientes, socios, mayoristas y fabricantes para contribuir en la innovación y desarrollo tecnológico.</p>
+                    </div>
+                </section>
+            </section>
+
+            <Footer />
         </Layout>
     )
 }
