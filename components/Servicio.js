4d37051e62f9a3e31847e3d3ad12e39dca8a8d43
Canos13 <canomsergio13@gmail.com>
2022-08-17 18:52:17 -0500

    solo un componente

--- components/Servicio.js
+++ components/Servicio.js
@@ -2,10 +2,10 @@ import { Stack } from '@mui/system'
 import { GoCloudDownload } from 'react-icons/go'
 import { ColorButton } from '../lib/theme'
 
-const Servicio = ({t1, p1, a1, img1, t2, p2, a2, img2}) => {
+const Servicio = ({t1, p1, a1, img1, clase}) => {
     return (
         <>
-            <section className="servicio">
+            <section className={clase}>
                 <section className="servicio-texto">
                     <h1>{t1}</h1>
                     <p>{p1}</p>
@@ -17,20 +17,6 @@ const Servicio = ({t1, p1, a1, img1, t2, p2, a2, img2}) => {
                     </a>
                 </section>
                 <img src={img1} />
-            </section>
-
-            <section className="servicio">
-                <img src={img2} />
-                <section className="servicio-texto">
-                    <h1>{t2}</h1>
-                    <p>{p2}</p>
-                    <a href={a2} download className="decoration-none">
-                        <Stack spacing={3} direction="row" >
-                            <ColorButton variant="contained" className='mx-auto' endIcon={<GoCloudDownload className='ml-2' />}>
-                                Descargar Folleto</ColorButton>
-                        </Stack>
-                    </a>
-                </section>
             </section>
         </>
     )
