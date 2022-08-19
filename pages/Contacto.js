6e637d569adfeab18dce2c48d6267a72bc879120
Canos13 <canomsergio13@gmail.com>
2022-08-19 09:50:22 -0500

    importamos el mapa de manera dinamica

--- pages/Contacto.js
+++ pages/Contacto.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Fade } from "react-awesome-reveal";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+import dynamic from 'next/dynamic';
+
+const Mapa = dynamic(
+  () => import("../components/Mapa"),
+  { ssr: false }
+)
 
 const Contacto = () => {
+
   return (
-    <div>Contacto</div>
+    <Layout>
+        <header className='contacto-header'>
+            {/* Barra de navegación */}
+            <Navbar />
+
+            {/* Encabezado */}
+            <section className="hero_container container">
+                <Fade direction="right">
+                    <h1 className="hero_title">Contacto</h1>
+                </Fade>
+            </section>
+        </header>
+
+        <Mapa />
+
+        <Footer />
+    </Layout>
   )
 }
 
