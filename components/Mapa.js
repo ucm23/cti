8c498eadd7122b9126852fa2d381f81a2a8c3662
Canos13 <canomsergio13@gmail.com>
2022-08-21 22:24:06 -0500

    espaciado

--- components/Mapa.js
+++ components/Mapa.js
@@ -4,25 +4,25 @@ import 'leaflet/dist/leaflet.css';
 const position = [19.4406049398052, -99.19983385061217];
 
 const Mapa = () => {
-  return (
-    <MapContainer
-        center={position}
-        zoom={17}
-        scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    return (
+        <MapContainer
+            center={position}
+            zoom={17}
+            scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-      <Marker position={position}>
-          <Popup>
-            Aquí nos puedes encontrar
-          </Popup>
-      </Marker>
-    
-    </MapContainer>
-  )
+          <Marker position={position}>
+              <Popup>
+                Aquí nos puedes encontrar
+              </Popup>
+          </Marker>
+        
+        </MapContainer>
+    )
 }
 
 export default Mapa
