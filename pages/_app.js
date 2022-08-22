8968018a76272dfc7dd76844b0bffcb2ee188eaa
Canos13 <canomsergio13@gmail.com>
2022-08-21 22:24:23 -0500

    documentacion de _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../assets/global.css';
 import '../assets/normalize.css'
 
+/**
+ * This function takes in a Component and pageProps as arguments and returns a Component with
+ * pageProps.
+ * returns The component that is being rendered.
+ */
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
