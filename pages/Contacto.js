6c0fe96718cdbb90e1cbe421d6b0dc16cbb76716
Canos13 <canomsergio13@gmail.com>
2022-08-30 11:40:24 -0500

    con Fade

--- pages/Contacto.js
+++ pages/Contacto.js
@@ -7,7 +7,7 @@ import { HiLocationMarker, HiMail, HiPhoneOutgoing } from 'react-icons/hi'
 import Formulario from "../components/Formulario";
 
 const Mapa = dynamic(() => import("../components/Mapa"),{ 
-    ssr: false 
+    ssr: false,
 })
 
 const Contacto = () => {
@@ -25,59 +25,68 @@ const Contacto = () => {
                 </Fade>
             </section>
         </header>
-
-        <h1 className="titulo_mapa">Nos Ubicamos en</h1>
-        <section className="area-mapa contenedor">
-            <Mapa />
-            <div className="area-contacto">
-                <div className="contacto-cont">
-                    <div className="contacto1">
-                        <div className="contacto_logo">
-                            <HiLocationMarker className="contacto_ico contacto_HiLocationMarker" />
-                        </div>
-                        <div className="contacto_texto">
-                            <div className="contacto_titulo">
-                                Ubicación
+        
+        <div className="overhid">
+            <Fade direction="right">
+                <div>
+                    <h1 className="titulo_mapa">Nos Ubicamos en</h1>
+                    <section className="area-mapa contenedor">
+                        <Mapa />   
+                        <div className="area-contacto">
+                            <div className="contacto-cont">
+                                <div className="contacto1">
+                                    <div className="contacto_logo">
+                                        <HiLocationMarker className="contacto_ico contacto_HiLocationMarker" />
+                                    </div>
+                                    <div className="contacto_texto">
+                                        <div className="contacto_titulo">
+                                            Ubicación
+                                        </div>
+                                        <div className="contacto_parrafo">
+                                            Av. Ejército Nacional 769, 769, piso 2. Col. Granada C.P. 11520
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="contacto1">
+                                    <div className="contacto_logo">
+                                    <HiMail className="contacto_ico contacto_HiMail" />
+                                    </div>
+                                    <div className="contacto_texto">
+                                        <div className="contacto_titulo">
+                                            Correo
+                                        </div>
+                                        <div className="contacto_parrafo">
+                                            contacto@consultores.cti.com.mx<br /> gsamano@orbnote.com
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="contacto1">
+                                    <div className="contacto_logo">
+                                        <HiPhoneOutgoing className="contacto_ico contacto_HiPhoneOutgoing" />
+                                    </div>
+                                    <div className="contacto_texto">
+                                        <div className="contacto_titulo">
+                                            Teléfono
+                                        </div>
+                                        <div className="contacto_parrafo">
+                                            811 588 03 05 <br /> 55 54 54 76 22
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="contacto_parrafo">
-                                Av. Ejército Nacional 769, 769, piso 2. Col. Granada C.P. 11520
-                            </div>
                         </div>
-                    </div>
-                    <div className="contacto1">
-                        <div className="contacto_logo">
-                          <HiMail className="contacto_ico contacto_HiMail" />
-                        </div>
-                        <div className="contacto_texto">
-                            <div className="contacto_titulo">
-                                Correo
-                            </div>
-                            <div className="contacto_parrafo">
-                                contacto@consultores.cti.com.mx<br /> gsamano@orbnote.com
-                            </div>
-                        </div>
-                    </div>
-                    <div className="contacto1">
-                        <div className="contacto_logo">
-                            <HiPhoneOutgoing className="contacto_ico contacto_HiPhoneOutgoing" />
-                        </div>
-                        <div className="contacto_texto">
-                            <div className="contacto_titulo">
-                                Teléfono
-                            </div>
-                            <div className="contacto_parrafo">
-                                811 588 03 05 <br /> 55 54 54 76 22
-                            </div>
-                        </div>
-                    </div>
+                    </section>
                 </div>
-            </div>
-        </section>
+            </Fade>
+        </div>
 
-        <section className="contenedor">
-            <section className="my-25">
-                <Formulario />
-            </section>
+
+        <section className="contenedor overhid">
+            <Fade direction="left">
+                <section className="my-25">
+                    <Formulario />
+                </section>
+            </Fade>
         </section>
 
         <Footer />
