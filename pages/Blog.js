c142753e349b4210ff457c65ee257882ad4b3ca0
Canos13 <canomsergio13@gmail.com>
2022-08-30 19:14:25 -0500

    mapeamos el array

--- pages/Blog.js
+++ pages/Blog.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Blogdata } from '../lib/services'
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+    return (
+        <section className='blog cont'>
+          <div className='conteni topMarign'>
+            <div className='heading'>
+              <h3>LATEST BLOG</h3>
+              <h1>Read Inspirational Story Every Week</h1>
+            </div>
+
+            <div className='contain grid topMarign'>
+                {
+                  Blogdata.map((val) => {
+                    return (
+                      <div className='box'>
+                        <div className='img'>
+                          <img src={val.cover} alt='' />
+                        </div>
+                        <div className='text'>
+                          <span>{val.date}</span>
+                          <h2>{val.title}</h2>
+                          <p>{val.desc}</p>
+                          <a href='/'>
+                            Leer más...
+                          </a>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+            </div>
+          </div>
+        </section>
+    )
 }
 
 export default Blog
