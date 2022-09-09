680300cff6a4d2273f5484daec841a2f16af8072
canos13 <canomsergio13@gamil.com>
2022-09-08 19:50:15 -0500

    quitamos espacios

--- components/Mapa.js
+++ components/Mapa.js
@@ -11,17 +11,15 @@ const Mapa = () => {
             zoom={17}
             scrollWheelZoom={false}
         >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          <Marker position={position}>
-              <Popup>
-                Aquí nos puedes encontrar
-              </Popup>
-          </Marker>
-        
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    Aquí nos puedes encontrar
+                </Popup>
+            </Marker>
         </MapContainer>
     )
 }
